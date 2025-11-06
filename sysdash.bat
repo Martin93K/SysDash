abebaab7ce378cfbb0e427259056b2ddeb0f5b67
Martin93K <ck313383@fi365.ort.edu.uy>
2025-11-05 22:28:51 -0300

    fixed terminal size errors

--- sysdash.bat
+++ sysdash.bat
@@ -5,9 +5,8 @@ if %errorLevel% neq 0 (
     powershell -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
-echo ✅ Ejecutando como administrador...
+echo Ejecutando como administrador...
 
 title SysDash 
-powershell -command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{F11}')}" >nul 2>&1
 "C:\Program Files\Git\bin\bash.exe" -c "cd "ruta de instalación" && ./menu.sh"
 pause
